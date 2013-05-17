a92fae9a1c49ad495ee4708ac271bf601ad5845c
rzoller <roman.zoller@epfl.ch>
2013-05-17 19:19:26 +0200

    Add TouchPressType MOVE, create custom chimpchat jar

--- WebScreenInterface/src/MultiTouchSample.java
+++ WebScreenInterface/src/MultiTouchSample.java
@@ -1,7 +1,8 @@
 import com.android.chimpchat.adb.AdbBackend;
 import com.android.chimpchat.core.IChimpDevice;
+import com.android.chimpchat.core.TouchPressType;
 
-public class MonkeyTest {
+public class MultiTouchSample {
 	public static void main(String[] args) {
 		// sdk/platform-tools has to be in PATH env variable in order to find adb
 		IChimpDevice device = new AdbBackend().waitForConnection();
@@ -12,6 +13,24 @@ public class MonkeyTest {
 		// Take a snapshot and save to out.png
 		device.takeSnapshot().writeToFile("out.png", null);
 		
+		for (int i = 0; i < 10; i++) {
+			device.touch(90, 265, TouchPressType.DOWN);
+			sleep(50);
+			device.touch(150, 265, TouchPressType.MOVE);
+			sleep(50);
+			device.touch(150, 365, TouchPressType.MOVE);
+			sleep(50);
+			device.touch(150, 365, TouchPressType.UP);
+			sleep(1000);
+		}
+		
 		device.dispose();
+	}
+	
+	public static void sleep(long ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+		}
 	}
 }
